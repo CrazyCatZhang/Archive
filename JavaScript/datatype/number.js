fd5545a22ff478213e054d60b58b4a4ba67492c0
Peach Zhang <CrazyCatZhang@outlook.com>
2020-09-10 23:03:21 +0800

    Create number.js

---
+++ JavaScript/datatype/number.js
@@ -0,0 +1,7 @@
+let a = 123;
+let b = "123";
+console.log(typeof a);
+console.log(typeof b);
+console.log(Number.MAX_VALUE);
+console.log(Number.MAX_VALUE * Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
