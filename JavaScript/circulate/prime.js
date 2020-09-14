c6f5e6542858784181e8392f4caf21a53aa90ccd
Peach Zhang <CrazyCatZhang@outlook.com>
2020-09-15 00:07:44 +0800

    Create prime.js

---
+++ JavaScript/circulate/prime.js
@@ -0,0 +1,13 @@
+let flag = true;
+for (let i = 2; i < 100; i++) {
+    flag = true;
+    for (let j = 2; j < Math.sqrt(i); j++) {
+        if (i % j === 0) {
+            flag = false;
+            break;
+        }
+    }
+    if (flag) {
+        console.log(i);
+    }
+}
