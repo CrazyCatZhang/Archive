29137eed73c7dcf2ecdb10548894bcf1c5783428
Peach Zhang <CrazyCatZhang@outlook.com>
2020-09-15 10:11:05 +0800

    Update prime.js

--- JavaScript/circulate/prime.js
+++ JavaScript/circulate/prime.js
@@ -1,3 +1,4 @@
+console.time("test");
 for (let i = 2; i < 100; i++) {
     let flag = true;
     for (let j = 2; j < Math.sqrt(i); j++) {
@@ -10,3 +11,4 @@ for (let i = 2; i < 100; i++) {
         console.log(i);
     }
 }
+console.timeEnd("test");
