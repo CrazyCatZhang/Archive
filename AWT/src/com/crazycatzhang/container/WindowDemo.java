2c8540ae4aa237767a1b201513f43083fa7a025c
CrazyCatZhang <CrazyCatZhang@outlook.com>
2023-01-25 19:20:32 +0800

    Window container demo

---
+++ AWT/src/com/crazycatzhang/container/WindowDemo.java
@@ -0,0 +1,12 @@
+package com.crazycatzhang.container;
+
+import java.awt.*;
+
+public class WindowDemo {
+    public static void main(String[] args) {
+        Frame frame = new Frame("WindowDemo");
+        frame.setLocation(100, 100);
+        frame.setSize(500, 400);
+        frame.setVisible(true);
+    }
+}
