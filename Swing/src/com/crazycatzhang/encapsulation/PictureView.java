e575201164b77e68d398c661dff9b15ec6a3298f
CrazyCatZhang <CrazyCatZhang@outlook.com>
2023-02-23 12:10:07 +0800

    Optimized packaging

--- Swing/src/com/crazycatzhang/encapsulation/PictureView.java
+++ Swing/src/com/crazycatzhang/encapsulation/PictureView.java
@@ -1,7 +1,10 @@
 package com.crazycatzhang.encapsulation;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.InputStream;
 
 public class PictureView extends JPanel {
     private Image image;
@@ -15,6 +18,26 @@ public class PictureView extends JPanel {
     public void setImage(Image image) {
         this.image = image;
         this.repaint();
+    }
+
+    public void setImage(File file) {
+        try {
+            this.image = ImageIO.read(file);
+            this.repaint();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setImage(String resourcePath) {
+        try {
+            InputStream res = this.getClass().getResourceAsStream(resourcePath);
+            assert res != null;
+            this.image = ImageIO.read(res);
+            this.repaint();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
