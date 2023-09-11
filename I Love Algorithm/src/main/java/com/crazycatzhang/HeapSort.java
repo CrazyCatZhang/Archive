3a46b6430640ce0a7064d2b32961038d03761e34
CrazyCatZhang <CrazyCatZhang@outlook.com>
2023-09-11 21:34:04 +0800

    HeapSort

---
+++ I Love Algorithm/src/main/java/com/crazycatzhang/HeapSort.java
@@ -0,0 +1,58 @@
+package com.crazycatzhang;
+
+/**
+ * @author crazycatzhang
+ */
+public class HeapSort {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 2, 5, 2, 6, 4, 7};
+        heapSort(arr);
+        for (int i :
+                arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void heapSort(int[] arr) {
+        if (arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            heapInsert(arr, i);
+        }
+        int heapSize = arr.length;
+        swap(arr, 0, --heapSize);
+        while (heapSize > 0) {
+            heapIfy(arr, 0, heapSize);
+            swap(arr, 0, --heapSize);
+        }
+    }
+
+    public static void heapInsert(int[] arr, int index) {
+        while (arr[index] > arr[(index - 1) / 2]) {
+            swap(arr, index, (index - 1) / 2);
+            index = (index - 1) / 2;
+        }
+    }
+
+    public static void heapIfy(int[] arr, int index, int heapSize) {
+        int left = index * 2 + 1;
+        while (left < heapSize) {
+            int largest = left + 1 < heapSize && arr[left + 1] > arr[left] ? left + 1 : left;
+            largest = arr[index] > arr[largest] ? index : largest;
+            if (largest == index) {
+                break;
+            }
+            swap(arr, largest, index);
+            index = largest;
+            left = index * 2 + 1;
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
