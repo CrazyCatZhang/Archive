ae789fcad196eeedd08c2a6b765981525753412f
CrazyCatZhang <CrazyCatZhang@outlook.com>
2023-09-18 22:58:41 +0800

    TraversalBinaryTree

---
+++ I Love Algorithm/src/main/java/com/crazycatzhang/TraversalBinaryTree.java
@@ -0,0 +1,120 @@
+package com.crazycatzhang;
+
+import java.util.Stack;
+
+/**
+ * @author crazycatzhang
+ */
+public class TraversalBinaryTree {
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.left = new Node(2);
+        head.right = new Node(3);
+        head.left.left = new Node(4);
+        head.left.right = new Node(5);
+        head.right.left = new Node(6);
+        head.right.right = new Node(7);
+
+        preUnRecursive(head);
+        System.out.println("========");
+        inUnRecursive(head);
+        System.out.println("========");
+        posUnRecursive(head);
+        System.out.println("========");
+    }
+
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int v) {
+            value = v;
+        }
+    }
+
+    public static void pre(Node head) {
+        if (head == null) {
+            return;
+        }
+        System.out.println(head.value);
+        pre(head.left);
+        pre(head.right);
+    }
+
+    public static void in(Node head) {
+        if (head == null) {
+            return;
+        }
+        in(head.left);
+        System.out.println(head.value);
+        in(head.right);
+    }
+
+    public static void pos(Node head) {
+        if (head == null) {
+            return;
+        }
+        pos(head.left);
+        pos(head.right);
+        System.out.println(head.value);
+    }
+
+    public static void preUnRecursive(Node head) {
+        if (head == null) {
+            return;
+        }
+        Stack<Node> stack = new Stack<>();
+        stack.push(head);
+        while (!stack.isEmpty()) {
+            head = stack.pop();
+            System.out.println(head.value);
+            if (head.right != null) {
+                stack.push(head.right);
+            }
+            if (head.left != null) {
+                stack.push(head.left);
+            }
+        }
+    }
+
+    public static void inUnRecursive(Node head) {
+        if (head == null) {
+            return;
+        }
+        Stack<Node> stack = new Stack<>();
+        while (!stack.isEmpty() || head != null) {
+            if (head != null) {
+                stack.push(head);
+                head = head.left;
+            } else {
+                head = stack.pop();
+                System.out.println(head.value);
+                head = head.right;
+            }
+        }
+    }
+
+    public static void posUnRecursive(Node head) {
+        if (head == null) {
+            return;
+        }
+        Stack<Node> stack1 = new Stack<>();
+        Stack<Node> stack2 = new Stack<>();
+        stack1.push(head);
+        while (!stack1.isEmpty()) {
+            head = stack1.pop();
+            stack2.push(head);
+            if (head.left != null) {
+                stack1.push(head.left);
+            }
+            if (head.right != null) {
+                stack1.push(head.right);
+            }
+        }
+        while (!stack2.isEmpty()) {
+            System.out.println(stack2.pop().value + " ");
+        }
+    }
+}
